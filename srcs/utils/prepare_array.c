bb16b9e80e2dc6eec2717ee5d938d4932d3a400e
jedelfos <jerome.delfosse.78@gmail.com>
2021-01-28 16:49:34 +0100

    nouveaut parcing par type fin

--- srcs/utils/prepare_array.c
+++ srcs/utils/prepare_array.c
@@ -6,7 +6,7 @@
 /*   By: jedelfos <jedelfos@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 11:41:28 by thjacque          #+#    #+#             */
-/*   Updated: 2021/01/27 17:00:49 by jedelfos         ###   ########lyon.fr   */
+/*   Updated: 2021/01/28 16:49:05 by jedelfos         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ void	fill_teub(char **teub, char *line, t_all *a)
 	a->flag_quote = 0;
 	a->flag_esc = 0;
 	a->flag_cmd = 0;
-//	ft_printf("reset\n");
 	get_blocks(teub, line, &j);
 }
 
@@ -30,11 +29,13 @@ void	set_value(char ***teub, int cmds, int *i, int *j)
 	*j = -1;
 }
 
-int		create_struc(char *line, int i, char type, t_type *str_type)
+t_type	*create_struc(char *line, int i, char type, t_type *str_type)
 {
-	int y;
+	int		y;
 	t_type	*temp;
+	t_type	*save_first;
 
+	save_first = str_type;
 	y = 0;
 	temp = wrmalloc(sizeof(t_type));
 	temp->content = wrmalloc(sizeof(char) * (i + 2));
@@ -45,49 +46,97 @@ int		create_struc(char *line, int i, char type, t_type *str_type)
 	}
 	temp->content[y] = 0;
 	temp->type = type;
-	ft_printf("-+-|%s|-+- %i\n",temp->content,temp->type);
+	temp->next = NULL;
+	if (str_type == NULL)
+		return (temp);
+	while (str_type->next != NULL)
+		str_type = str_type->next;
+	str_type->next = temp;
+	return (save_first);
+}
 
-	if (str_type != NULL)
-		str_type->next = temp;
+char	check_type(char *str, int i, char c)
+{
+	if (c == '1')
+		return (0);
+	if (str[i] == '>' && str[i + 1] == '>')
+		return (5);
+	else if (i > 0 && (str[i] == '>' && str[i - 1] == '>'))
+		return (5);
+	else if (str[i] == '<')
+		return (4);
+	else if (str[i] == '>')
+		return (3);
+	else if (str[i] == '|')
+		return (2);
+	else if (str[i] == ';')
+		return (1);
+	return (0);
+}
+
+int		jump_cote(char *line, int i, char *line_p)
+{
+	char	c;
+
+	if ((line[i] == '\'' || line[i] == '\"') && line_p[i] == '0')
+	{
+		c = line[i];
+		i++;
+		while (line[i] && (!(line[i] == c && line_p[i] == '0')))
+			i++;
+		if (!(line[i]))
+			return (-1);
+	}
 	return (i);
 }
 
-int		split_type(char *line)
+t_type	*split_type(char *line, char *line_p)
 {
-	int i;
-	int j;
-	t_type	*str_type;
+	t_type	*f_type;
+	int		i;
+	char	type;
+	int		u;
 
-	str_type = NULL;
-	int u;
-	u = -1;
-	j = 0;
+	f_type = NULL;
+	u = 0;
 	i = 0;
+	type = check_type(line, 0, '0');
+	while (line[0] && line[i])
+	{
+		type = check_type(line, i, line_p[i]);
+		if ((i = jump_cote(line, i + 1, line_p)) == -1)
+			return (0);
+		if (line_p[i] == '0' && check_type(line, i, '0') != type)
+		{
+			f_type = create_struc(line + u, i - u - 1,
+				check_type(line, i - 1, '0'), f_type);
+			u = i;
+		}
+	}
+	if (u != i)
+		create_struc(line + u, i - u - 1, check_type(line, i - 1, '0'), f_type);
+	wrfree(line_p);
+	return (f_type);
+}
+
+char	*line_pre(char *line)
+{
+	int		i;
+	char	*line_temp;
+
+	i = 0;
+	line_temp = wrmalloc(ft_strlen(line) + 1);
+	line_temp[i] = '0';
 	while (line[i])
 	{
-
-		if (j != 0 && line[i] == j)
-			j = 0;
-		else if (line[i] == '\"' || line[i] == '\'')
-			j = line[i];
-		else if (j == 0 && (line[i] == '>' && line[i + 1] == '>'))
-			u = create_struc(line + u, i - u, 5, str_type);
-*/		else if (j == 0 && line[i] == '<')
-			u = create_struc(line + u + 1, i - u - 1, 4, str_type) + u + 1;	
-		else if (j == 0 && line[i] == '>')
-			u = create_struc(line + u + 1, i - u - 1, 3, str_type) + u + 1;
-		else if (j == 0 && line[i] == '|')
-			u = create_struc(line + u + 1, i - u - 1, 2, str_type) + u + 1;
-		else if (j == 0 && line[i] == ';')
-			u = create_struc(line + u + 1, i - u - 1, 1, str_type) + u + 1;
-//			create_struc(line + u, i - u, 1, str_type);
+		if (line[i] == '\\' && line_temp[i] != '1')
+			line_temp[i + 1] = '1';
+		else
+			line_temp[i + 1] = '0';
 		i++;
 	}
-	if (((i - 1) - u) > 0 && line[u + 1] )
-		create_struc(line + u + 1, (i - 2) - u, 4, 0);
-
-	ft_printf("\n");
-	return (0);
+	line_temp[i] = 0;
+	return (line_temp);
 }
 
 char	***prepare_array(char *line, t_all *all)
@@ -97,6 +146,20 @@ char	***prepare_array(char *line, t_all *all)
 	int		i;
 	int		j;
 	int		args;
+
+
+//	LAUCHER DE MA FONCTION
+	t_type	*first_type;
+
+	first_type = split_type(line, line_pre(line));
+	while (first_type)
+	{
+		ft_printf("%s               %i\n", first_type->content, first_type->type);
+		first_type = first_type->next;
+	}
+// RESET DE LINE
+	line = strdup("");
+
 
 	if ((cmds = splitter_counter_cmd(line, all, -1)) < 0)
 		return (NULL);
@@ -114,9 +177,6 @@ char	***prepare_array(char *line, t_all *all)
 		if (!(teub[i][args] = wrmalloc(1 * sizeof(char*))))
 			ft_exit(MALLOC);
 		teub[i][args] = 0;
-
-		split_type(line);
-
 		fill_teub(teub[i], line, all);
 		handler(teub[i], all, get_env_st(NULL), 0);
 	}
